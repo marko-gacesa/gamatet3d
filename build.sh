74828a3d07f4fc4caec1d10a544a3b0e53ce76e9
Marko Gaćeša <markogacesa@hotmail.com>
2026-05-05 10:05:02 +0200

    CMake and gitignore

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-g++ -g -O0 -Wall gamatet.cpp tet3d.cpp menu.cpp anim.cpp brick.cpp brickfield.cpp timer.cpp -o gmt3d -lGL -lGLU -lglut
+#g++ -g -O0 -Wall -Werror gamatet.cpp tet3d.cpp menu.cpp anim.cpp brick.cpp brickfield.cpp timer.cpp -o gamatet3d -lGL -lGLU -lglut
+g++ -s -O3 -Wall -Werror gamatet.cpp tet3d.cpp menu.cpp anim.cpp brick.cpp brickfield.cpp timer.cpp -o gamatet3d -lGL -lGLU -lglut
