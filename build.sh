ced93f0c2d53dc13ea99a3593f258e62d89aa8f5
Marko Gaćeša <markogacesa@hotmail.com>
2026-05-07 21:33:44 +0200

    remove build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-# Debug build
-#g++ -g -O0 -Wall -Werror gamatet.cpp tet3d.cpp menu.cpp anim.cpp brick.cpp brickfield.cpp timer.cpp -o gamatet3d -lGL -lglut
-
-# Release build
-g++ -s -O3 -Wall -Werror gamatet.cpp tet3d.cpp menu.cpp anim.cpp brick.cpp brickfield.cpp timer.cpp -o gamatet3d -lGL -lglut
